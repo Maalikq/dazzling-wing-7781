0c2954251b21243da2626e3e8554e8f2849cd8c6
Shubham Verma <imkannu.shubham@gmail.com>
2022-08-27 23:37:40 +0530

    add

---
+++ checkout.js
@@ -0,0 +1,4 @@
+import { navbar, footer_main, end } from "../dazzling-wing-7781/components/navbar";
+document.getElementById("navbar").innerHTML = navbar();
+document.getElementById("footer_1").innerHTML = footer_main();
+document.getElementById("footer_end").innerHTML = end();
